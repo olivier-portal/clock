fc6ecf111b5b86a21948d7a762750e0261564b30
MrRobot <olivier.portal@laplateforme.io>
2025-01-09 16:29:45 +0100

    bug: fix main
    Fixed the problem of dual launching of set_time and display_time

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ from functions.update_time import update_time
 from functions.set_alarm import set_alarm
 import time
 
+# permitt to switch between functions
+# enable_display_time = True
+# enable_set_time = False
 # run functions in order of appearence
-# display_time()
-set_time()
+
+while True:
+    user_input = input("Voulez-vous mettre l'horloge à lheure ? O/N ")
+    if user_input == "o" or user_input == "O":
+        set_time()
+    elif user_input == "n" or user_input == "N":
+        display_time()
+    else:
+        print("Vous devez entrer O ou N")
+        
+
+
