5026061f4874414880f957bfe3fddf3b11a0ab7c
MrRobot <olivier.portal@laplateforme.io>
2025-01-09 10:18:59 +0100

    bug: True

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from functions.update_time import update_time
 from functions.set_alarm import set_alarm
 import time
 
-set_alarm()
 current_time = set_time()
 while True:
     current_time = update_time(current_time)
