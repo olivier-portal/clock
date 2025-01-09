297a24326aec4618ef16b0f4dada7b88b9277881
MrRobot <olivier.portal@laplateforme.io>
2025-01-09 15:04:28 +0100

    bug: fix display time

--- functions/update_time.py
+++ functions/update_time.py
@@ -1,5 +1,4 @@
-def update_time(current_time):
-    
+def update_time(current_time):        
     while True:
         hours, minutes, seconds = current_time
         seconds += 1
@@ -14,26 +13,5 @@ def update_time(current_time):
         
         if hours >=24:
             hours = 0
-        while True:
-            print(f"{hours:02}:{minutes:02}:{seconds:02}", end="\r")
             
-            return (hours, minutes, seconds)
-        
-    # while True:
-    #     hours, minutes, seconds = current_time
-    #     seconds += 1
-        
-    #     if seconds >=60:
-    #         seconds = 0
-    #         minutes += 1
-            
-    #     if minutes >=60:
-    #         minutes = 0
-    #         hours += 1
-        
-    #     if hours >=24:
-    #         hours = 0
-    #     while True:
-    #         print(f"{hours:02}:{minutes:02}:{seconds:02}", end="\r")
-            
-    #         return (hours, minutes, seconds)
+        return (hours, minutes, seconds)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# import functions to run them
+from functions.display_time import display_time
 from functions.set_time import set_time
 from functions.update_time import update_time
 from functions.set_alarm import set_alarm
 import time
 
-current_time = set_time()
-while True:
-    current_time = update_time(current_time)
-    time.sleep(1)
+# run functions in order of appearence
+current_time = display_time()
+set_time(current_time)
