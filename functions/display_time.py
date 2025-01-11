640edb2f57aa9044e6ba175309e057f86200eb89
MrRobot <olivier.portal@laplateforme.io>
2025-01-11 18:57:33 +0100

    feat: add 12h 24h handling code

--- functions/display_time.py
+++ functions/display_time.py
@@ -8,22 +8,22 @@ def display_time():
     seconds = 0
     current_time = (hours, minutes, seconds)
     
+    # Add a loop to update the new hour every second
     pause = False
-    
     while True:
         if pause == False:
             current_time = update_time(current_time)
-            print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02}", "hold f to put time on pause      ", end="\r")
+            print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02}", "Appuyez sur p pour mettre en pause    ", end="\r")
             time.sleep(1)
-            
-        if pause == True:# pause fonctionnel
-            print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02}", "it's paused, press g to restart", end="\r")
         
-        if keyboard.is_pressed("f"):
+        if pause == True:# activate pause
+            print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02}", "En pause, appuyez sur r pour reprendre", end="\r")
+        
+        if keyboard.is_pressed("p"):
             pause = True
             time.sleep(0.01)
             
-        if pause == True and keyboard.is_pressed("g"): 
+        if pause == True and keyboard.is_pressed("r"): 
             pause = False
             time.sleep(0.01)
         
