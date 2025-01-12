0cd28e94f76e6025b829fef49ae432728e87890d
MrRobot <olivier.portal@laplateforme.io>
2025-01-12 14:30:16 +0100

    bug: fix am_pm bug

--- functions/set_time.py
+++ functions/set_time.py
@@ -51,12 +51,12 @@ def set_time():
                 print("Veuillez entrer un nombre valide pour les secondes.")
         
         am_pm = input("AM ou PM ? (entrez AM/PM) : ").strip().upper()
-        if am_pm == "PM" and hours != 12:
-            hours += 12
+        if am_pm == "PM" and hours == 11 and minutes == 59 and seconds == 59:
+            hours = 0
         elif am_pm == "AM" and hours == 12:
             hours = 0
 
-        current_time = (hours, minutes, seconds)
+        current_time = (hours, minutes, seconds, am_pm)
 
     # manage 24h format
     elif time_format_input == 2:
@@ -93,21 +93,32 @@ def set_time():
         current_time = (hours, minutes, seconds)
     
     
-    # Add a loop to update the new hour every second
+    # Add a loop to update the new hour every second and manage pause
     pause = False
     while True:
+        '''Add a possibility to put time on pause'''
+        # time is running
         if pause == False:
-            current_time = update_time(current_time)
-            print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02}", "Appuyez sur p pour mettre en pause    ", end="\r")
+            current_time = update_time(current_time, time_format_input)
+            if time_format_input == 1:
+                print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02} {current_time[3]}", "Appuyez sur 'p' pour mettre en pause", end="\r")
+            else:
+                print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02}", "Appuyez sur 'p' pour mettre en pause", end="\r")
             time.sleep(1)
         
-        if pause == True:# activate pause
-            print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02}", "En pause, appuyez sur r pour reprendre", end="\r")
+        # time is on pause
+        if pause == True:
+            if time_format_input == 1:
+                print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02} {current_time[3]}", "En pause, appuyez sur r pour reprendre", end="\r")
+            else:
+                print(f"{current_time[0]:02}:{current_time[1]:02}:{current_time[2]:02}", "En pause, appuyez sur r pour reprendre", end="\r")
         
+        # press p to put time on pause
         if keyboard.is_pressed("p"):
             pause = True
             time.sleep(0.01)
-            
+        
+        # press r to run time again    
         if pause == True and keyboard.is_pressed("r"): 
             pause = False
             time.sleep(0.01)
--- functions/update_time.py
+++ functions/update_time.py
@@ -1,21 +1,25 @@
-def update_time(current_time):        
-    while True:
-        # stock hours, minutes and seconds
+def update_time(current_time, time_format):        
+    if time_format == 1:
+        hours, minutes, seconds, am_pm = current_time
+    else:
         hours, minutes, seconds = current_time
-        
-        # add a second for update
-        seconds += 1
-        
-        # keep time in right format
-        if seconds >=60:
-            seconds = 0
-            minutes += 1
-            
-        if minutes >=60:
-            minutes = 0
-            hours += 1
-        
-        if hours >=24:
+
+    # Ajouter une seconde
+    seconds += 1
+    if seconds >= 60:
+        seconds = 0
+        minutes += 1
+    if minutes >= 60:
+        minutes = 0
+        hours += 1
+
+    if time_format == 1:  # Gestion du format 12h
+        if hours > 12:
+            hours = 1
+        if hours == 12 and minutes == 0 and seconds == 0:
+            am_pm = "PM" if am_pm == "AM" else "AM"
+        return (hours, minutes, seconds, am_pm)
+    else:  # Gestion du format 24h
+        if hours >= 24:
             hours = 0
-            
         return (hours, minutes, seconds)
